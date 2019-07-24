2e190ec01c28164d9da035d51b10de90c1a91667
befuddledyoungster <44422650+befuddledyoungster@users.noreply.github.com>
2019-07-24 21:46:25 +0530

    Grammar

--- high_LOW.py
+++ high_LOW.py
@@ -1,43 +1,43 @@
 import random
 def high_low():
-                print "wanna play high low"
-                a=raw_input("press y to begin")
+                print "Wanna play high low"
+                a=raw_input("Press y to begin")
                 if a=='y':
-                                c=input("which number do you like(1,9){this will decide number of digits in the number}")
+                                c=input("Which number do you like(1,9){this will decide number of digits in the number}")
                                 b=random.randint(10**(c-1),(10**c)-1)
-                print "instructions for the game are as follows"
-                print "you have to guess a number,rest you will understand"
+                print "Instructions for the game are as follows"
+                print "You have to guess a number,rest you will understand"
                 if c==1:
                                 g=3
-                                print "you have",3," chances to guess"
+                                print "You have",3," chances to guess"
                 elif c==2:
                                 g=5
-                                print "you have",5,"chances to guess"
+                                print "You have",5,"chances to guess"
                 elif c>2:
                                 g=c*4-2
-                                print "you have",(c*4-2),"chances to guess"
+                                print "You have",(c*4-2),"chances to guess"
                 e=0
                 print "ok ready"
                 while True:
-                                d=input("guess the c digit number")
+                                d=input("Guess the c digit number")
                                 if d>b:
                                                 e=e+1
-                                                print "too high"
+                                                print "Too high"
                                                 
                                 elif d<b:
                                                 e=e+1
-                                                print "too low"
+                                                print "Too low"
                                 if g-e==1:
                                                 print "1 try left"
                                 else:
-                                                print g-e,"tries left"
+                                                print g-e,"Tries left"
                                 if e==g:
-                                                print "you have lost the game"
-                                                print "the number is",b
+                                                print "You have lost the game"
+                                                print "The number is",b
                                                 break
                                 elif d==b:
-                                                print "you won the game"
-                                                print "congoooooooooooooooooooooo"
+                                                print "You won the game"
+                                                print "Congoooooooooooooooooooooo"
                                                 break
 high_low()
 
