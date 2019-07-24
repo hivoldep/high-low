f3ba7e6199c518f91fa46751773d9ba3c3730e3b
hivoldep <48147391+hivoldep@users.noreply.github.com>
2019-07-24 22:36:37 +0530

    Update high_LOW.py

--- high_LOW.py
+++ high_LOW.py
@@ -1,43 +1,65 @@
 import random
+def randomizer(c):
+    m=random.randint(1,100)
+    if m%3==0:
+        a=random.randint(1,3)
+    if m%3==1:
+        a=random.randint(4,6)
+    if m%3==2:
+        a=random.randint(7,9) 
+    for i in range(0,c-1):
+        b=random.randint(0,9)
+        a=a*10+b
+    return a
+
+def chances(c):
+    n=0
+    b=10**c
+    a=2
+    while 1!=0:
+        if (2**n)>b:
+            return n-1
+        n=n+1
+        
 def high_low():
                 print ("Wanna play high low")
                 a=raw_input("Press y to begin")
                 if a=='y':
                                 c=input("Which number do you like(1,9){this will decide number of digits in the number}")
-                                b=random.randint(10**(c-1),(10**c)-1)
+                                b=randomizer(c)
                 print ("Instructions for the game are as follows")
                 print ("You have to guess a number,rest you will understand")
                 if c==1:
-                                g=3
-                                print ("You have",3," chances to guess")
+                                g=chances(c)
+                                print ("You have",g" chances to guess")
                 elif c==2:
-                                g=5
-                                print ("You have",5,"chances to guess")
+                                g=chances(c)
+                                print ("You have",g"chances to guess")
                 elif c>2:
-                                g=c*4-2
-                                print ("You have",(c*4-2),"chances to guess")
+                                g=chances(c)
+                                print ("You have",g"chances to guess")
                 e=0
                 print "ok ready"
                 while True:
-                                d=input("Guess the c digit number")
+                               d=input("guess the c digit number")
                                 if d>b:
                                                 e=e+1
-                                                print ("Too high")
+                                                print ("too high")
                                                 
                                 elif d<b:
                                                 e=e+1
-                                                print ("Too low")
-                                if g-e==1:
-                                                print ("1 try left")
-                                else:
+                                                print ("too low")
+                                if g-e>1 and d!=b:
                                                 print (g-e,"tries left")
+                                if g-e==1 and d!=b:
+                                                print ("1 try left")
                                 if e==g:
-                                                print ("You have lost the game")
-                                                print ("The number is",b)
+                                                print ("you have lost the game")
+                                                print ("the number is",b)
                                                 break
-                                elif d==b:
-                                                print ("You won the game")
-                                                print ("Congoooooooooooooooooooooo")
+                                elif g-e!=0 and d==b:
+                                                print ("you won the game")
+                                                print ("congoooooooooooooooooooooo")
                                                 break
 high_low()
 
